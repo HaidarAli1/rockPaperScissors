43c27bfcbf1947f8bdf96ec78d84d7d1fa7c4ce9
Haidar Ali <shehrihaidarali@gmail.com>
2023-09-06 10:13:43 -0400

    feat: computer choice in RPS

---
+++ main.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+       let choice = Math.floor(Math.random() * 3) + 1;
+
+       if (choice == 1) {
+            return "rock";
+       } else if (choice == 2) {
+            return "paper";
+       }else if (choice == 3) {
+            return "scissors"
+       }else {
+            return undefined;
+       }
+}
+
+console.log(getComputerChoice());
